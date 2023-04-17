ef90be0b2ac59a3f6c48509987526e18154b4402
catalunha <catalunha.mj@gmail.com>
2023-04-17 11:59:37 -0300

    rev

---
+++ lib/app/core/repositories/attendance_repository.dart
@@ -0,0 +1,18 @@
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+
+import '../../data/b4a/table/attendance_b4a.dart';
+import '../../data/utils/pagination.dart';
+import '../models/attendance_model.dart';
+
+class AttendanceRepository {
+  final AttendanceB4a apiB4a = AttendanceB4a();
+
+  AttendanceRepository();
+  Future<List<AttendanceModel>> list(
+    QueryBuilder<ParseObject> query,
+    Pagination pagination,
+  ) =>
+      apiB4a.list(query, pagination);
+  Future<String> update(AttendanceModel model) => apiB4a.update(model);
+  Future<bool> delete(String modelId) => apiB4a.delete(modelId);
+}
